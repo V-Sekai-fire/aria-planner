67f84a417e37ca3a08a659fe1bbbc54178b41aad
Ernest Lee <elee@node-1w7jr9qvnpvfe3hl4xze952pi.ipv6.telus.net>
2025-12-14 13:01:34 -0800

    Remove aria_storage dependency to eliminate hackney requirement
    - Remove aria_storage from dependencies
    - This eliminates ex_aws, waffle, and hackney transitive dependencies
    - Improves OTP 26 compatibility by removing hackney dependency chain

--- mix.exs
+++ mix.exs
@@ -55,8 +55,6 @@ defmodule AriaPlanner.MixProject do
       # Added for SQLite3 Ecto adapter
       {:ecto_sqlite3, "~> 0.22.0"},
       {:aria_core, git: "https://github.com/V-Sekai-fire/aria-core.git"},
-      {:aria_storage,
-       git: "https://github.com/V-Sekai-fire/aria-storage.git", ref: "2ae9d51537a7272d489663c56206731312c961aa"},
       {:abnf_parsec, "~> 2.1"},
       {:sourceror, "~> 1.10"},
       # Using built-in :zstd module from Erlang/OTP 28+ (no external dependency needed)
